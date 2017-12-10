4c82389c878c6f5d444223714e64103fb3f07755
Caio Mateus <caiomateusgames@gmail.com>
2017-12-10 20:30:22 -0200

    'retirando-usuario'

--- src/main/java/com/example/algamoney/api/repository/UsuarioRepository.java
+++ src/main/java/com/example/algamoney/api/repository/UsuarioRepository.java
@@ -1,13 +1,13 @@
 package com.example.algamoney.api.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-List<Usuario> findByNomeLikeIgnoreCase(String nome);
-    
-    Usuario findByEmail(String email);
+
+	public Optional<Usuario> findByEmail(String email);
+	
 }
