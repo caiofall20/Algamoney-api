16a2724fef71a3a4b6fd9d96f7a87608b814ac4e
Caio Mateus <caiomateusgames@gmail.com>
2017-12-10 22:04:51 -0200

    'adicionando-usuario-sistema'

--- src/main/java/com/example/algamoney/api/repository/UsuarioRepository.java
+++ src/main/java/com/example/algamoney/api/repository/UsuarioRepository.java
@@ -1,16 +1,15 @@
 package com.example.algamoney.api.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	public Usuario findByEmail(String email);
 List<Usuario> findByNomeLikeIgnoreCase(String nome);
     
-    Usuario findByEmail(String email);
+   
 	    
 }
--- src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
+++ src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
@@ -2,13 +2,11 @@ package com.example.algamoney.api.security;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,28 +21,19 @@ public class AppUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	
-		
-		
-	
-	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username);
+        if (usuario == null) {
+            throw new UsernameNotFoundException(String.format("Usuário não existe!", username));
+        }
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
+	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
 		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
 		usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
 		return authorities;
-	}
-
-
-
-
-
-
-
-	@Override
-	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
